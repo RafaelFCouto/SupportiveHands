64180190b5b4a026fb8288044b98e39df940f437
RafaelFCouto <rafaelfreirecouto@outlook.com>
2024-11-23 11:57:38 -0400

    update script

--- src/scripts/start.sh
+++ src/scripts/start.sh
@@ -3,6 +3,9 @@
 # Aguardar o serviço de banco de dados (db) ficar disponível
 dockerize -wait tcp://db:3306 -timeout 60s
 
+# Instalar pacotes node
+npm install
+
 # Executar as migrações
 npm run supportive-hands-db:migrate:dev
 
