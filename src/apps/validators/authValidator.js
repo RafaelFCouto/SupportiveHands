9534b35b2783f665fd1ac48bca78b00756d973d8
RafaelFCouto <rafaelfreirecouto@outlook.com>
2024-12-03 15:53:21 -0400

    update authValidator

--- src/apps/validators/authValidator.js
+++ src/apps/validators/authValidator.js
@@ -9,15 +9,6 @@ const authSchema = Joi.object({
             'string.email': 'E-mail is not valid',
             'any.required': 'E-mail is required'
         }),
-    password: Joi.string()
-        .min(8)
-        .pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$'))
-        .required()
-        .messages({
-          'string.pattern.base': 'A senha deve ter pelo menos 8 caracteres, incluindo uma letra maiúscula, uma minúscula, um número e um caractere especial.',
-          'string.min': 'A senha deve ter pelo menos 8 caracteres.',
-          'any.required': 'O campo senha é obrigatório.',
-        }),
 });
 
 module.exports = authSchema;
