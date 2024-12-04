e9941870be24641934e6e32975822bcf92e1de8c
RafaelFCouto <rafaelfreirecouto@outlook.com>
2024-12-04 16:05:58 -0400

    Add authMiddleware on userRoute

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,10 +3,12 @@ const UserController = require('../apps/controllers/UserControllers');
 const createUserSchema = require('../apps/validators/user-validators/createUserValidator');
 const updateUserSchema = require('../apps/validators/user-validators/updateUserValidator');
 const ValidateMiddleware = require('../apps/middlewares/validate-middleware');
+const AuthMiddleware = require('../apps/middlewares/authentication');
 
 const router = Router();
 
 router.post('/users/create', ValidateMiddleware(createUserSchema), UserController.createUser);
+router.use(AuthMiddleware);
 router.put('/users/update', ValidateMiddleware(updateUserSchema), UserController.updateUser);
 router.delete('/users/delete', UserController.deleteUser);
 router.get('/users/:id', UserController.listUser);
