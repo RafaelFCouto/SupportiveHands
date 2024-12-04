b581877b0e041d9660b48fc1a6a97d50dc936618
RafaelFCouto <rafaelfreirecouto@outlook.com>
2024-12-04 15:59:56 -0400

    Implement authRoute

---
+++ src/routes/authRoute.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const AuthController = require('../apps/controllers/AuthControllers');
+const ValidateMiddleware = require('../apps/middlewares/validate-middleware');
+const authSchema = require('../apps/validators/authValidator');
+
+const router = Router();
+
+router.post('/auth', ValidateMiddleware(authSchema), AuthController.authenticate);
+
+module.exports = router;
