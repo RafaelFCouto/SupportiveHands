a67ca584239c5ff3bcc6b30b0345d93f0eede73f
RafaelFCouto <rafaelfreirecouto@outlook.com>
2024-12-12 18:35:50 -0400

    Adjustment on validators

--- src/apps/validators/institution-validators/institutionValidator.js
+++ src/apps/validators/institution-validators/institutionValidator.js
@@ -71,21 +71,6 @@ class institutionSchemas {
         });
     }
 
-    getInstitutionSchema = () => {
-        return Joi.object({
-            id: Joi.number()
-                .integer()
-                .min(1)
-                .required()
-                .messages({
-                    'number.base': 'O ID deve ser um número.',
-                    'number.integer': 'O ID deve ser um número inteiro.',
-                    'number.min': 'O ID deve ser maior ou igual a 1.',
-                    'any.required': 'O campo ID é obrigatório.',
-                })
-        });
-    }
-
     updateInstitutionSchema = () => {
         return Joi.object({
             name: Joi.string()
